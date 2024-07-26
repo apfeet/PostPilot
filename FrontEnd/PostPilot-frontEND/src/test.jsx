c649b6c88230fab7b49c2ebfa280386fef6f7722
apfeet <ottaviomalao@gmail.com>
2024-07-26 10:07:53 +0200

    Added the necessary thing to work properly

---
+++ FrontEnd/PostPilot-frontEND/src/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+      <h1>This is the Test page</h1>
+      <p>Here you can try your thing!</p>
+    </div>
+  )
+}
+
+export default Test
