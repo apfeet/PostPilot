ef8cc3548367bddce70e5a88545d2d91f21a31df
kavin00111 <developerkavinxx@gmail.com>
2024-08-02 18:57:21 +0530

    hour 1

---
+++ FrontEnd/PostPilot-frontEND/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../components/hero.css'
+import Logo from '../assets/LOGO.webp'
+
+const Hero = () => {
+  return (
+    <div className='header'>
+      <div className="logo"><img src={Logo} alt="PostPilot" /></div>
+      <button>about us</button>
+    </div>
+  )
+}
+
+export default Hero
--- FrontEnd/PostPilot-frontEND/src/main.jsx
+++ FrontEnd/PostPilot-frontEND/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
-import Test from './test.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   </React.StrictMode>,
